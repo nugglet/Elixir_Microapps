c778813857c3fceccd82d8e4c5d5d075fd39b9b6
miles <victoriayong@gmail.com>
2022-10-14 05:01:14 +0800

    feat(todo): added checkboxes and delete button functionality

--- todo/lib/todo_web/controllers/live_view.ex
+++ todo/lib/todo_web/controllers/live_view.ex
@@ -3,6 +3,7 @@ defmodule TodoWeb.TodoLive do
   alias Todo.Tasks
 
   def mount(_params, _session, socket) do
+    Todo.subscribe()
     {:ok, fetch(socket)}
   end
 
@@ -12,7 +13,24 @@ defmodule TodoWeb.TodoLive do
     {:noreply, fetch(socket)}
   end
 
+  def handle_event("toggle_done", %{"id" => id}, socket) do
+    task = Tasks.get_task!(id)
+    Tasks.update_task(task, %{done: !task.done})
+    {:noreply, fetch(socket)}
+  end
+
+  def handle_event("delete", %{"ref" => ref}, socket) do
+    task = Tasks.get_task!(ref)
+    Tasks.delete_task(task)
+    {:noreply, fetch(socket)}
+  end
+
   defp fetch(socket) do
     assign(socket, tasks: Tasks.list_tasks())
   end
+
+  def handle_info({Tasks, [:task | _], _}, socket) do
+    {:noreply, fetch(socket)}
+  end
+
 end
